a7ddc2d86033984e7c9628cac549e7b77fae71dc
Utku Can Yücel <utkucanyucel@gmail.com>
2016-02-16 16:36:44 +0000

    komit

--- src/email.go
+++ src/email.go
@@ -4,9 +4,9 @@ import (
     "fmt"
   	"net/http"
 
-  	// "appengine"
-   //  "appengine/log"
-  	// "appengine/mail"
+  	"appengine"
+    "appengine/log"
+  	"appengine/mail"
 )
 
 func init() {
@@ -16,23 +16,23 @@ func init() {
 
 func sendEmail(w http.ResponseWriter, r *http.Request) {
 				//  fmt.Println(“got:”, r.URL.Query());
-  			toEmail := r.URL.Query()["to"][0];
+//   			toEmail := r.URL.Query()["to"][0];
     		fmt.Fprint(w, "asdasddas")
   
-  			ctx := appengine.NewContext(r)
-        url := "asdasdasd"
-        msg := &mail.Message{
-                Sender:  "<utkucanyucel@gmail.com>",
-                To:      []string{toEmail},
-                Subject: "Bu Bir OTOMATIK E-postadır",
-                Body:    fmt.Sprintf("test", url),
-        }
-  			fmt.Fprint(w, "geldi")
-        if err := mail.Send(ctx, msg); err != nil {
-						log.Errorf(ctx, "Couldn't send email: %v", err)
-        }else{
-          	fmt.Fprint(w, "gonderildi.")
-        }
+//   			ctx := appengine.NewContext(r)
+//         url := "asdasdasd"
+//         msg := &mail.Message{
+//                 Sender:  "<utkucanyucel@gmail.com>",
+//                 To:      []string{toEmail},
+//                 Subject: "Bu Bir OTOMATIK E-postadır",
+//                 Body:    fmt.Sprintf("test", url),
+//         }
+//   			fmt.Fprint(w, "geldi")
+//         if err := mail.Send(ctx, msg); err != nil {
+// 						log.Errorf(ctx, "Couldn't send email: %v", err)
+//         }else{
+//           	fmt.Fprint(w, "gonderildi.")
+//         }
 }
 
 //test
