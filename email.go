61129cfb5e86215829cc92efb18381ff34bad7d3
tkcn <utkucanyucel@gmail.com>
2016-02-16 14:06:32 +0200

    initial commit

---
+++ email.go
@@ -0,0 +1,35 @@
+package hello
+
+import (
+    "fmt"
+  	"net/http"
+  	"google.golang.org/appengine"
+    "google.golang.org/appengine/log"
+  	"google.golang.org/appengine/mail"
+)
+
+func init() {
+  	http.HandleFunc("/send", sendEmail)
+}
+
+
+func sendEmail(w http.ResponseWriter, r *http.Request) {
+				//  fmt.Println(“got:”, r.URL.Query());
+  			toEmail := r.URL.Query()["to"][0];
+    		fmt.Fprint(w, "asdasddas")
+  
+  			ctx := appengine.NewContext(r)
+        url := "asdasdasd"
+        msg := &mail.Message{
+                Sender:  "<utkucanyucel@gmail.com>",
+                To:      []string{toEmail},
+                Subject: "Bu Bir OTOMATIK E-postadır",
+                Body:    fmt.Sprintf("test", url),
+        }
+  			fmt.Fprint(w, "geldi")
+        if err := mail.Send(ctx, msg); err != nil {
+						log.Errorf(ctx, "Couldn't send email: %v", err)
+        }else{
+          	fmt.Fprint(w, "gonderildi.")
+        }
+}
